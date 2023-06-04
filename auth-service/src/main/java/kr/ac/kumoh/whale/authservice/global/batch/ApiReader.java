d98b3e59bfa322b9d8e36a7c0ad02a6625ec201d
Jeonwoojae <dnwo0719@icloud.com>
2023-06-04 19:15:45 +0900

    [feat]: 산업재해 중대산업사고 발생사업장 정보 저장

--- auth-service/src/main/java/kr/ac/kumoh/whale/authservice/global/batch/ApiReader.java
+++ auth-service/src/main/java/kr/ac/kumoh/whale/authservice/global/batch/ApiReader.java
@@ -3,6 +3,7 @@ package kr.ac.kumoh.whale.authservice.global.batch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.ac.kumoh.whale.authservice.global.batch.json.accident_workplace.AccidentWorkplace;
 import kr.ac.kumoh.whale.authservice.global.batch.json.barrier_free_certified_workplace.BarrierFreeCertifiedWorkplace;
 import kr.ac.kumoh.whale.authservice.global.batch.json.health_center.HealthCenterInfo;
 import kr.ac.kumoh.whale.authservice.global.batch.json.risk_assessment_certified_workplace.RiskAssessmentCertifiedWorkplace;
@@ -30,6 +31,52 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ApiReader {
     private String serviceKey = "vdGJEuz%2B3h6ABcRGeQGy8ZFlf9w4QYyvY4pCW3PD4YrofQFod1ylz6kX870R98FprfaItjyPu1Y1V%2BCNjGkn6Q%3D%3D";
+
+    @Bean
+    public ItemReader<AccidentWorkplace> accidentWorkplaceItemReader() throws URISyntaxException, JsonProcessingException {
+        List<AccidentWorkplace> accidentWorkplaces = getFileItemReaderFromAccidentWorkplaceApi(AccidentWorkplace.class);
+        return new IteratorItemReader<>(accidentWorkplaces);
+    }
+
+    private List<AccidentWorkplace> getFileItemReaderFromAccidentWorkplaceApi(Class<AccidentWorkplace> accidentWorkplaceClass) throws URISyntaxException, JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        int currentPage = 0;
+        int currentCount = 1000;
+        int totalCount = 1001;
+
+        List<AccidentWorkplace> result = new ArrayList<>();
+
+        while (currentCount!=0){
+            currentPage++;
+            log.info("accident workplace info totalCount : {}, currentPage : {}, currentCount : {}",totalCount, currentPage, currentCount);
+            String uri = "https://api.odcloud.kr/api/15090011/v1/uddi:df4cf98f-feea-4c0e-8139-891536632192?"
+                    + "page=" + currentPage + "&perPage=" + 1000 + "&serviceKey=" + serviceKey;
+            URI uri1 = new URI(uri);
+
+            //log.info("Fetching data from an external API by using the url: {}", uri);
+
+            ResponseEntity<String> response = restTemplate.exchange(uri1, HttpMethod.GET,
+                    new HttpEntity<>(headers), String.class);
+
+            // Json parsing
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> responseObject = objectMapper.readValue(response.getBody(),
+                    new TypeReference<Map<String, Object>>() {});
+
+            ArrayList<Map<String,?>> dataProperty = (ArrayList<Map<String, ?>>) responseObject.get("data");
+            List<AccidentWorkplace> accidentWorkplaces = dataProperty.stream().map((Map<String, ?> t) -> new AccidentWorkplace(t)).collect(Collectors.toList());
+
+            currentCount = Integer.parseInt(responseObject.get("currentCount").toString());
+            currentPage = Integer.parseInt(responseObject.get("page").toString());
+            totalCount = Integer.parseInt(responseObject.get("totalCount").toString());
+            result.addAll(accidentWorkplaces);
+        }
+
+        return result;
+    }
 
     @Bean
     public ItemReader<BarrierFreeCertifiedWorkplace> barrierFreeCertifiedWorkplaceItemReader() throws URISyntaxException, JsonProcessingException {
--- auth-service/src/main/java/kr/ac/kumoh/whale/authservice/global/batch/FileItemReaderJobConfig.java
+++ auth-service/src/main/java/kr/ac/kumoh/whale/authservice/global/batch/FileItemReaderJobConfig.java
@@ -2,6 +2,8 @@ package kr.ac.kumoh.whale.authservice.global.batch;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.ac.kumoh.whale.authservice.domain.jobs.dto.JobAnnouncementDto;
+import kr.ac.kumoh.whale.authservice.global.batch.json.accident_workplace.AccidentWorkplace;
+import kr.ac.kumoh.whale.authservice.global.batch.json.accident_workplace.AccidentWorkplaceApiWriter;
 import kr.ac.kumoh.whale.authservice.global.batch.json.barrier_free_certified_workplace.BarrierFreeCertifiedWorkplace;
 import kr.ac.kumoh.whale.authservice.global.batch.json.barrier_free_certified_workplace.BarrierFreeCertifiedWorkplaceApiWriter;
 import kr.ac.kumoh.whale.authservice.global.batch.json.health_center.HealthCenterApiWriter;
@@ -41,6 +43,9 @@ public class FileItemReaderJobConfig {
 
     // 배리어프리 인증사업장 정보
     private final BarrierFreeCertifiedWorkplaceApiWriter barrierFreeCertifiedWorkplaceApiWriter;
+
+    // 산업재해 중대산업사고 발생 사업장
+    private final AccidentWorkplaceApiWriter accidentWorkplaceApiWriter;
 
     @Bean
     public Job jobAnnouncementCsvFileItemReaderJob() {
@@ -103,6 +108,22 @@ public class FileItemReaderJobConfig {
                 .<BarrierFreeCertifiedWorkplace, BarrierFreeCertifiedWorkplace>chunk(chunkSize)
                 .reader(apiReader.barrierFreeCertifiedWorkplaceItemReader())
                 .writer(barrierFreeCertifiedWorkplaceApiWriter)
+                .build();
+    }
+
+    @Bean
+    public Job accidentWorkplaceApiReaderJob() throws JsonProcessingException, URISyntaxException {
+        return jobBuilderFactory.get("accidentWorkplaceApiReaderJob")
+                .start(accidentWorkplaceApiReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step accidentWorkplaceApiReaderStep() throws JsonProcessingException, URISyntaxException {
+        return stepBuilderFactory.get("accidentWorkplaceApiReaderStep")
+                .<AccidentWorkplace, AccidentWorkplace>chunk(chunkSize)
+                .reader(apiReader.accidentWorkplaceItemReader())
+                .writer(accidentWorkplaceApiWriter)
                 .build();
     }
 }
